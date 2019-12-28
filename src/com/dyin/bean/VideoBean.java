5f22b112646c8d1319662b3d13d5f282309adc3f
lyk7539511 <852486850@qq.com>
2019-12-28 14:49:43 +0800

    add videobean

---
+++ src/com/dyin/bean/VideoBean.java
@@ -0,0 +1,19 @@
+package com.dyin.bean;
+
+public class VideoBean {
+	private int id;
+	private String video;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getVideo() {
+		return video;
+	}
+	public void setVideo(String video) {
+		this.video = video;
+	}	
+
+}
